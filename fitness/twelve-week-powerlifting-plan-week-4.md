378d62f50e84e56526b219c63829dee6818b4696
pythonicqualms <pythonicqualms@gmail.com>
2022-10-24 08:13:52 -0500

    update artwork srl wrapper

---
+++ fitness/twelve-week-powerlifting-plan-week-4.md
@@ -0,0 +1,26 @@
+---
+title: "A 12-week Fitness Plan - Week 4"
+date: "2022-10-24"
+tags:
+  - fitness
+  - powerlifting
+---
+
+# A 12-Week Powerlifting Plan
+
+This is my 4th week of a plan I started on `2022-10-3`.
+
+## Week 4
+
+### 2022-10-24
+
+| DAY 13              | SETS | REPS | WEIGHT | DAILY VOLUME |
+|---------------------|------|------|--------|--------------|
+| SQUAT               | 1    | 4    | 320    | 1280         |
+| SQUAT               | 3    | 4    | 295    | 3540         |
+| INCLINE BENCH PRESS | 1    | 5    | 165    | 825          |
+| INCLINE BENCH PRESS | 1    | 5    | 155    | 775          |
+| INCLINE BENCH PRESS | 2    | 8    | 145    | 2320         |
+| WEIGHTED PISTOLS    | 3    | 5    | 25     | 375          |
+| WEIGHTED PULLUPS    | 3    | 8    | 52     | 1248         |
+| LEG RAISES          | 3    | 8    | 0      | -            |
--- pages/artwork.js
+++ pages/artwork.js
@@ -1,54 +1,68 @@
-import Layout from "../components/layout";
-import { SRLWrapper } from "simple-react-lightbox";
-import { getSortedPhotoData } from "../components/artwork";
+import Layout from '../components/layout'
+import { SRLWrapper } from 'simple-react-lightbox'
+import { getSortedPhotoData } from '../components/artwork'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPhotoData();
+export async function getStaticProps () {
+  const allPostsData = getSortedPhotoData()
   return {
     props: {
-      allPostsData,
-    },
-  };
+      allPostsData
+    }
+  }
 }
 
 const lightBoxOptions = {
   buttons: {
-    showDownloadButton: true,
+    showDownloadButton: false
   }
 }
 
-function BlurImage({ imageData }) {
+function BlurImage ({ imageData }) {
   return (
-    <SRLWrapper options={lightBoxOptions}>
-      <a href={`/images/${imageData.id}.jpg`} data-attribute="SRL" className="group">
-        <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-          <img src={`/images/${imageData.id}.jpg`} alt={imageData.id} />
-        </div>
-        <h3 className="mt-4 text-xs text-gray-300">{imageData.id}</h3>
-        <p className="mt-1 text-sm font-medium text-gray-700">
-          {imageData.date}
-        </p>
-      </a>
-    </SRLWrapper>
-  );
+    <a
+      id={imageData.id}
+      href={`/images/${imageData.id}.jpg`}
+      data-attribute='SRL'
+      className='group'
+    >
+      <div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
+        <img
+          id={imageData.id}
+          src={`/images/${imageData.id}.jpg`}
+          alt={imageData.id}
+        />
+      </div>
+      <h3 className='mt-4 text-xs text-gray-300'>{imageData.id}</h3>
+      <p className='mt-1 text-sm font-medium text-gray-700'>{imageData.date}</p>
+    </a>
+  )
 }
 
-export default function Artwork({ allPostsData }) {
+export default function Artwork ({ allPostsData }) {
   return (
     <Layout>
       <section>
-        <h2 className="text-lg text-center">artwork over the years</h2>
-        <h3 className="text-sm">Most of this is pen & ink on wood of some sort</h3>
+        <h2 className='text-lg text-center'>artwork over the years</h2>
+        <h3 className='text-sm'>
+          Most of this is pen & ink on wood of some sort
+        </h3>
       </section>
-      <SRLWrapper>
-        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {allPostsData.map((imageData) => {
-              return <BlurImage imageData={imageData}></BlurImage>;
+      <SRLWrapper
+        options={{
+          settings: { disablePanzoom: true, usingPreact: true },
+          buttons: {
+            showDownloadButton: false
+          }
+        }}
+      >
+        <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
+          <div className='grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
+            {allPostsData.map(imageData => {
+              return <BlurImage imageData={imageData}></BlurImage>
             })}
           </div>
         </div>
       </SRLWrapper>
     </Layout>
-  );
+  )
 }
