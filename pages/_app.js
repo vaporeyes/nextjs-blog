c2506efd28c6d7461e48e4a315bbd9621c857c28
jsh dncn <pythonicqualms@gmail.com>
2025-04-06 07:34:31 -0500

    no simple react lightbox

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,16 @@
 import 'highlight.js/styles/tokyo-night-dark.css'
 import '../styles/global.css'
-import SimpleReactLightbox from 'simple-react-lightbox'
 import { ThemeProvider } from 'next-themes'
 import styles from '../components/layout.module.css'
 import { ThemeSwitcher } from '../components/themeSwitcher'
 
 export default function App ({ Component, pageProps }) {
   return (
-    <SimpleReactLightbox>
       <ThemeProvider attribute="class">
         <div className={styles.lightButton}>
           <ThemeSwitcher />
         </div>
         <Component {...pageProps} />
-      </ThemeProvider>
-    </SimpleReactLightbox>
+      </ThemeProvider>  
   )
 }
